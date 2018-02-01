7b3fd386a7df393258523b896c43c91d95225a88
Chandler Keyes <chandler.keyes@helloworld.com>
2018-02-01 15:42:36 -0500

    Getting user input (commands)

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ const os = require('os');
 //since this is our own file, we give it the path of the file
 //if it was a core node module, you'd just give the name
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+if(command === 'list') {
+  console.log('Listing notes');
+  } else if(command === 'add') {
+      console.log('Adding note');
+  } else if(command === 'read') {
+      console.log('Reading note');
+  } else if(command == 'remove') {
+      console.log('Removing note');
+  } else {
+      console.log('Did not recognize command');
+  }
